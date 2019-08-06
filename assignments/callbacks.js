2067abe81c030f8ab38af772177bd76bf1787281
Miley Wright <dishaliekwright@yahoo.com>
2019-08-06 09:30:19 -0400

    working last question in callbacks.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -27,24 +27,45 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+    return cb(arr.length)
 }
+getLength(items, function(length){
+  console.log(length)
+});
+
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[3])
 }
+last(items, function(lastitem){
+  console.log(lastitem)
+});
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y)
 }
-
+sumNums(4,6, function(sum){
+  console.log(sum)
+})
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y)
 }
+multiplyNums(2,4,(sum => console.log(sum)))
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  return cb(list.filter(function(list){
+    return item === list;
+  }
+  .map(function(list){
+    return console.log("true")
+  })))
 }
+contains(3,3, (test => console.log(test)))
 
 /* STRETCH PROBLEM */
 
